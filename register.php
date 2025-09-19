0b3d8a439cd544462f754cc31976e642fbf8ecb6
Hirenknu <hirenknu@gmail.com>
2025-09-19 22:36:48 +0530

    Add files via upload

---
+++ register.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
+
+	<title></title>
+ </head>
+<body>
+	<div class= "main-container">
+		<section>
+			<div class ="form-container" id="admin_login">
+				<form action="" method="post" enctype="multipart/form-data">
+					<h3> Register now </h3>
+					<div> </div>
+				</form>
+
+			</div>
+		</section>
+	</div>
+
+
+
+<!-- sweetalert cdn link -->
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
+
+<!-- custom js link -->
+
+	<script type="text/javascript" src="script.js"></script>
+</body>
+</html>
